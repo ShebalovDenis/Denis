2c5dc63a771430415755655f4327d33b43403383
Vadim <wadim-ka95@mail.ru>
2016-10-30 18:45:09 +0300

    Создал вторую ветку

---
+++ src/JavaWork/Bubble.java
@@ -0,0 +1,30 @@
+package JavaWork;
+
+public class Bubble {
+	public static void main(String[] args){
+		int[] arr = new int[10];
+		
+		 
+		 for(int i = 0; i < arr.length; i++){
+		   arr[i] = (int)(Math.random()*100); // это случайными 100
+		  // System.out.println(arr[i]);
+		}
+		
+		doAlgoritm(arr);
+		
+		
+		}
+		public static void doAlgoritm(int[] arr)
+	    {	
+		for(int i = 0; i < arr.length-1; i++){
+			for(int j = i+1 ; j < arr.length-1; j++){
+				if (arr[i]>arr[j]){
+				arr[i] = arr[i] + arr[j] - (arr[j] = arr[i]);			
+				}
+			}
+			System.out.print(arr[i]+ " ");
+		}
+		
+			
+		}
+}
