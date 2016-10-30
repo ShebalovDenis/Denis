f663213853cb2ad9a2ef9451a1fe469e095782af
Vadim <wadim-ka95@mail.ru>
2016-10-30 18:46:38 +0300

    Готово!

--- src/JavaWork/Bubble.java
+++ src/JavaWork/Bubble.java
@@ -1,6 +1,20 @@
 package JavaWork;
 
 public class Bubble {
+	public static void main(String[] args){
+		int[] arr = new int[10];
+		
+		 
+		 for(int i = 0; i < arr.length; i++){
+		   arr[i] = (int)(Math.random()*100); // это случайными 100
+		  // System.out.println(arr[i]);
+		}
+		
+		doAlgoritm(arr);
+		
+		
+		}
+	
 	public static void doAlgoritm(int[] arr)
     {	
 	for(int i = 0; i < arr.length-1; i++){
